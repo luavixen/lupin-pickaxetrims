34fe197cb3f7e591d68b6715a8ab2d8f764c2125
Lua MacDougall <lua@foxgirl.dev>
2024-10-28 15:45:06 -0700

    Attempt to add mixinextras to fix crash

--- common/src/main/java/dev/foxgirl/pickaxetrims/shared/mixin/quartz_bonus_experience_effect/MixinBlock.java
+++ common/src/main/java/dev/foxgirl/pickaxetrims/shared/mixin/quartz_bonus_experience_effect/MixinBlock.java
@@ -7,11 +7,13 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Block.class)
+@Debug(export = true, print = true)
 public abstract class MixinBlock {
 
     @ModifyArg(
@@ -39,5 +41,44 @@ public abstract class MixinBlock {
         }
         return experience;
     }
+
+    /*
+    @Unique
+    private ItemStack pickaxetrims$local$dropExperienceWhenMined$tool;
+
+    @Inject(
+        method = "dropExperienceWhenMined(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/math/intprovider/IntProvider;)V",
+        at = @At("HEAD")
+    )
+    private void pickaxetrims$injected$dropExperienceWhenMined$HEAD(
+        ServerWorld world,
+        BlockPos pos,
+        ItemStack tool,
+        IntProvider experience,
+        CallbackInfo info
+    ) {
+        pickaxetrims$local$dropExperienceWhenMined$tool = tool;
+    }
+
+    @ModifyArg(
+        method = "dropExperienceWhenMined(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/math/intprovider/IntProvider;)V",
+        at = @At(
+            target = "Lnet/minecraft/block/Block;dropExperience(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;I)V",
+            value = "INVOKE"
+        ),
+        index = 2
+    )
+    private int pickaxetrims$injected$dropExperienceWhenMined$INVOKE$dropExperience(int experience) {
+        if (PickaxeTrim.isOfTrimType(pickaxetrims$local$dropExperienceWhenMined$tool, PickaxeTrim.TrimType.QUARTZ)) {
+            if (experience > 0) {
+                float multiplierMin = PickaxeTrimsImpl.getInstance().config.quartzExperienceMultiplierMin;
+                float multiplierMax = PickaxeTrimsImpl.getInstance().config.quartzExperienceMultiplierMax;
+                float multiplier = (float) (multiplierMin + Math.random() * (multiplierMax - multiplierMin));
+                return (int) (experience * multiplier);
+            }
+        }
+        return experience;
+    }
+    */
 
 }
